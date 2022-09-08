27099a4dc9f14f1c34af3a1b994f2dcc462c2ccd
Cristopher Paiva <josuepaiva7@gmail.com>
2022-09-08 13:13:53 -0300

    Was add enviroment variables

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import app from './src/app.js';
 const port = process.env.PORT || 3001;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
   console.log(`Srvidor escutando em http://localhost:${port}`);
 });
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://cristopherpds:root@cluster0.n3aeyjh.mongodb.net/desafio-api-compasso");
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.n3aeyjh.mongodb.net/desafio-api-compasso`);
 
 let db = mongoose.connection;
 
